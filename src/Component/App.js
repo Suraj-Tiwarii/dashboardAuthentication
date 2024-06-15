33cf9bf28fb30769be21919dfee571334eaf9c53
Suraj-Tiwarii <66522583+Suraj-Tiwarii@users.noreply.github.com>
2024-06-16 00:13:52 +0530

    Add files via upload

---
+++ src/Component/App.js
@@ -0,0 +1,29 @@
+// App.js
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
+import "./App.css";
+import Login from "./Component/Login";
+import Register from "./Component/Registration";
+import Dashboard from "./Component/Dashboard";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
